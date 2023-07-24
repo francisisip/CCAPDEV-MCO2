08c470be9b79d60225f309c4064d6e3236e7169a
gzachr <zachary_gomez@dlsu.edu.ph>
2023-07-24 21:59:23 +0800

    created user view

--- src/routes/users.js
+++ src/routes/users.js
@@ -15,11 +15,15 @@ router.get('/', (req, res) => {
 router.get("/:userID", async (req, res)=>{ 
 
     let activeID
+    let valid = 0
 
     await currUser.findOne({}).then(doc => {
         activeID = doc.get("userID", Number)
     })
 
+    if(activeID === req.params.userID){
+        valid = 1
+    }
 
     const userProfile = await User.findOne({ 
         userID: req.params.userID
@@ -27,18 +31,62 @@ router.get("/:userID", async (req, res)=>{
 
     const postsArray = await Post.find({
         userID: req.params.userID
-    })
+    }).lean()
 
     const commentsArray = await Comment.find({
         userID: req.params.userID
-    })
+    }).lean()
 
-    console.log(userProfile)
+    for(let post of postsArray) {
+        post.author = await User.findOne({userID: post.userID}).select('username profileImg').lean()
+        post.commentsNo = post.comments.length
+
+        let tag
+        switch(post.tag) {
+            case "Announcements":
+                tag = "bi-megaphone-fill";
+                break;
+            case "Review":
+                tag = "bi-check-circle-fill";
+                break;
+            case "Query":
+                tag = "bi-question-circle-fill";
+                break;
+            case "Meme":
+                tag = "bi-emoji-laughing-fill";
+                break;
+            case "Meta":
+                tag = "bi-lightbulb-fill";
+        }
+
+        if(post.upvoteList.includes(activeID)){
+            post.uvoteClass = 'upvote-2'
+            post.dvoteClass = 'downvote'
+        }
+        else if(post.downvoteList.includes(activeID)){
+            post.uvoteClass = 'upvote'
+            post.dvoteClass = 'downvote-2'
+        }
+        else {
+            post.uvoteClass = 'upvote'
+            post.dvoteClass = 'downvote'
+        }
+
+        post.tagClass = tag
+        post.voteCount = post.upvoteList.length - post.downvoteList.length
+    }
+
+    for(let comment of commentsArray) {
+        comment.author = await User.findOne({userID: comment.userID}).select('username profileImg').lean()
+    }
+
     if(userProfile){
         res.render("users", {
             user: activeID,
+            match: valid,
             loadedProfile: userProfile,
             posts: postsArray,
+            comments: commentsArray,
             helpers: {toLower, calcDate}
         });
     }
