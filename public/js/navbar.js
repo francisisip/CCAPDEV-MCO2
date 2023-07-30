0d7ad2be90f83479f0c9e3ef268859330ee266fb
wailingwailord <maximusburce@gmail.com>
2023-07-30 17:13:35 +0800

    all search buttons should work (except vote)
    note: lazy implementation, only ONE filter will work at a time (i.e. you cannot combine two or more filters, including with a basic search query itself) sry

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,4 +1,14 @@
 const logout = document.getElementById("logout-button")
+const searchBtn = document.getElementById("search-icon-btn-nav")
+const searchInput = document.getElementById("search-box-nav")
+
+searchBtn.addEventListener("click", async e => {
+    const input = searchInput.value.trim();
+    console.log(input);
+
+    const searchURL = `/search?search=${encodeURIComponent(input)}`;
+    window.location.href = searchURL;
+})
 
 logout.addEventListener("click", async e => {
     e.preventDefault()
--- public/js/searchQuery.js
+++ public/js/searchQuery.js
@@ -1,21 +1,12 @@
-window.addEventListener("load", function(e){
-    let searchButton = document.getElementById("search-icon-container")
-    let searchInput = this.document.querySelector('#search-box')
+const searchButton = document.getElementById("search-icon-btn")
+const searchBox = document.getElementById("search-box")
 
-    searchButton.addEventListener("click", async e =>{
-        e.preventDefault()
-        
-        let query = searchInput.value;
+searchButton.addEventListener("click", async e =>{
+    e.preventDefault()
+    
+    const input = searchBox.value.trim();
+    console.log(input);
 
-        const response = await fetch(`/search?search=${searchValue}`, {
-            method: 'GET',
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        });
-
-        // Handle the response from the back-end (e.g., update the UI with search results)
-        const searchData = await response.json();
-        // Implement the UI update based on the search results (e.g., display search results)
-    })
+    const searchURL = `/search?search=${encodeURIComponent(input)}`;
+    window.location.href = searchURL;
 })
