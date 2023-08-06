2722975b4cbd33666659ba040ecb6122d7c35619
Francis Isip <francis_bawa@dlsu.edu.ph>
2023-08-07 02:24:50 +0800

    fixed register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -72,9 +72,7 @@ router.post('/login', async (req,res)=>{
 
 router.post("/register", async (req, res) => {
     console.log("POST request received for /register");
-
     try {
-        console.log(errors)
 
         const validEmail = validateEmail(req.body.email);
         const emailExist = await User.findOne({email: req.body.email});
